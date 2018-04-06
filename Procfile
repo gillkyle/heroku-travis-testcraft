b71adcd6db25414e634afc441a963a1a8db11a14
Kyle Gill <kylerobertgill@gmail.com>
2018-04-06 10:30:42 -0600

    heroku procfile and shell script

---
+++ Procfile
@@ -0,0 +1,1 @@
+release: bash ./hello-world.sh
---
+++ hello-world.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\033[1m Hello World"
+ # bold effect
+echo -e "\033[0m Hello World"
+ # back to normal
+
+echo -e "\033[31m Hello World"
+ # Red color
+echo -e "\033[32m Hello World"
+ # Green color
+echo -e "\033[33m Hello World"
+ # See remaing on screen
+echo -e "\033[34m Hello World"
+echo -e "\033[35m Hello World"
+echo -e "\033[36m Hello World"
+echo -e "\033[0m This should run after a Heroku release"
